872857ab2958a141d923dd74478a4bdc561ee33a
Rodion Glukhov <public.rodion@icloud.com>
2020-09-28 22:22:10 +0400

    Реструктуризация настроек

--- api/time_tracker/settings.py
+++ api/time_tracker/settings.py
@@ -1,9 +1,29 @@
 """Настройки проекта."""
+from pathlib import Path
+
 from pydantic import BaseSettings
 
-
-class SettingsStorage(BaseSettings):
-    """Хранилище настроек из переменных окружения и .env файла."""
+BASE_DIRECTORY_PATH = Path(__file__).absolute().parent.parent
 
 
-storage = SettingsStorage()
+class DatabaseSettings(BaseSettings):
+    """Настройки баз данных."""
+
+    class Config:
+        """Мета конфигурация настроек."""
+
+        env_file = str(BASE_DIRECTORY_PATH / '.database.env')
+        env_file_encoding = 'utf-8'
+
+
+class ApplicationSettings(BaseSettings):
+    """Прочие настройки приложения."""
+
+    class Config:
+        """Мета конфигурация настроек."""
+
+        env_file = str(BASE_DIRECTORY_PATH / '.env')
+        env_file_encoding = 'utf-8'
+
+
+database_settings, application_settings = DatabaseSettings(), ApplicationSettings()
