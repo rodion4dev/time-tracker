ca9acd963696e42d892d8c5ccef415022d7b873a
Rodion Glukhov <public.rodion@icloud.com>
2020-10-03 21:46:12 +0400

    Добавление настроек БД

--- api/time_tracker/settings.py
+++ api/time_tracker/settings.py
@@ -1,13 +1,22 @@
 """Настройки проекта."""
+from ipaddress import IPv4Address
 from pathlib import Path
+from typing import Union
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, PositiveInt, RedisDsn, SecretStr
 
 BASE_DIRECTORY_PATH = Path(__file__).absolute().parent.parent
 
 
 class DatabaseSettings(BaseSettings):
     """Настройки баз данных."""
+
+    redis_dsn: RedisDsn
+    mysql_host: Union[str, IPv4Address] = 'localhost'
+    mysql_port: PositiveInt = '3306'
+    mysql_user: str
+    mysql_password: SecretStr
+    mysql_database: str
 
     class Config:
         """Мета конфигурация настроек."""
