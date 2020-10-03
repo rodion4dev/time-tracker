dd66710d63345285cb944eba53ad55e5c71713bf
Rodion Glukhov <public.rodion@icloud.com>
2020-10-04 01:26:41 +0400

    Форматирование кода

--- api/time_tracker/databases.py
+++ api/time_tracker/databases.py
@@ -11,10 +11,16 @@ async def init_mysql(application: Application):
     settings: _DatabaseSettings = application['database_settings']
     minsize, maxsize = settings.mysql_connections_count
     mysql_pool: aiomysql.Pool = await aiomysql.create_pool(
-        minsize=minsize, maxsize=maxsize, host=settings.mysql_host,
-        user=settings.mysql_user, password=settings.mysql_password.get_secret_value(),
-        db=settings.mysql_database, port=settings.mysql_port, charset='utf8',
-        program_name='time_tracker')
+        minsize=minsize,
+        maxsize=maxsize,
+        host=settings.mysql_host,
+        user=settings.mysql_user,
+        password=settings.mysql_password.get_secret_value(),
+        db=settings.mysql_database,
+        port=settings.mysql_port,
+        charset='utf8',
+        program_name='time_tracker',
+    )
     application['mysql_pool'] = mysql_pool
 
 
@@ -30,7 +36,8 @@ async def init_redis(application: Application):
     settings: _DatabaseSettings = application['database_settings']
     minsize, maxsize = settings.redis_connections_count
     redis: aioredis.Redis = await aioredis.create_redis_pool(
-        settings.redis_dsn, encoding='utf-8', minsize=minsize, maxsize=maxsize)
+        settings.redis_dsn, encoding='utf-8', minsize=minsize, maxsize=maxsize
+    )
     application['redis_pool'] = redis
 
 
