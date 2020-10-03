410105619a7b274f6d4bbcb54c7d53ed29c588d9
Rodion Glukhov <public.rodion@icloud.com>
2020-10-04 02:37:23 +0400

    Добавление typing

--- api/time_tracker/views.py
+++ api/time_tracker/views.py
@@ -1,5 +1,5 @@
 """Набор обработчиков HTTP запросов."""
-from aiohttp.web_response import json_response
+from aiohttp.web_response import Response, json_response
 from aiohttp.web_routedef import RouteTableDef
 from aiohttp.web_urldispatcher import View
 
@@ -11,32 +11,32 @@ routes = RouteTableDef()
 class LogView(View):
     """Взаимодействие с логами."""
 
-    async def get(self):
+    async def get(self) -> Response:
         """Получение логов."""
-        return json_response({})
+        return json_response(data={})
 
 
 @routes.view('/mysql-data')
 class MySQLData(View):
     """Взаимодействие с данными в MySQL базе."""
 
-    async def get(self):
+    async def get(self) -> Response:
         """Получение данных из MySQL."""
-        return json_response({})
+        return json_response(data={})
 
-    async def post(self):
+    async def post(self) -> Response:
         """Сохранение данных в MySQL."""
-        return json_response({})
+        return json_response(data={})
 
 
 @routes.view('/redis-data')
 class RedisData(View):
     """Взаимодействие с данными в Redis базе."""
 
-    async def get(self):
+    async def get(self) -> Response:
         """Получение данных из Redis."""
-        return json_response({})
+        return json_response(data={})
 
-    async def post(self):
+    async def post(self) -> Response:
         """Сохранение данных в Redis."""
-        return json_response({})
+        return json_response(data={})
