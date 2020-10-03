f19c9da68051ca93d13e8d4d66b6bb6756f70fcc
Rodion Glukhov <public.rodion@icloud.com>
2020-10-04 01:38:06 +0400

    Форматирование кода

--- api/time_tracker/application.py
+++ api/time_tracker/application.py
@@ -9,6 +9,7 @@ from time_tracker.settings import application_settings, database_settings
 async def create_application() -> Application:
     """Создание WEB приложения."""
     application = Application()
+
     application['settings'] = application_settings
     application['database_settings'] = database_settings
     application.add_routes(views.routes)
