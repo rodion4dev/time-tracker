2c208952ad2522a97b4e97832175486f30e88df2
Rodion Glukhov <public.rodion@icloud.com>
2020-10-04 00:52:27 +0400

    Подготовка соединения с mysql

--- api/time_tracker/databases.py
+++ api/time_tracker/databases.py
@@ -6,14 +6,19 @@ from time_tracker.settings import _DatabaseSettings
 
 
 async def init_mysql(application: Application):
-    """Подготовка MySQL базы данных."""
+    """Подготовка MySQL."""
     settings: _DatabaseSettings = application['database_settings']
-    connections_pool: aiomysql.Pool = await aiomysql.create_pool()
+    minsize, maxsize = settings.mysql_connections_count
+    connections_pool: aiomysql.Pool = await aiomysql.create_pool(
+        minsize=minsize, maxsize=maxsize, host=settings.mysql_host,
+        user=settings.mysql_user, password=settings.mysql_password.get_secret_value(),
+        db=settings.mysql_database, port=settings.mysql_port, charset='utf8',
+        program_name='time_tracker')
     application['mysql_connections_pool'] = connections_pool
 
 
 async def close_mysql(application: Application):
-    """Закрытие соединения с MySQL базой данных."""
+    """Закрытие соединений MySQL."""
     connections_pool: aiomysql.Pool = application['mysql_connections_pool']
     connections_pool.close()
     await connections_pool.wait_closed()
