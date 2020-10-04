6731f00fe76283763a5495dfadffcfebe0694f7c
Rodion Glukhov <public.rodion@icloud.com>
2020-10-04 20:17:27 +0400

    Сохранение данных в redis

--- api/time_tracker/views.py
+++ api/time_tracker/views.py
@@ -6,6 +6,7 @@ from aiohttp.web_response import Response, json_response
 from aiohttp.web_routedef import RouteTableDef
 from aiohttp.web_urldispatcher import View
 from aioredis import Redis
+from aioredis.commands import StringCommandsMixin
 
 routes = RouteTableDef()
 
@@ -48,4 +49,11 @@ class RedisData(View):
 
     async def post(self) -> Response:
         """Сохранение данных в Redis."""
+        data = await self.request.json()
+        if not isinstance(data, dict):
+            return json_response(data={'error': 'Ожидался объект с данными.'})
+
+        redis: Redis = self.request.app['redis']
+        for key, value in data.items():
+            redis.set(key, value, exist=StringCommandsMixin.SET_IF_NOT_EXIST)
         return json_response(data={})
